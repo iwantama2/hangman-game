3d98aa3a1d7f358a96ef79eca22fd27a7dd9c6e9
Mas Iwan <46395434+iwantama2@users.noreply.github.com>
2024-02-15 15:21:00 +0700

    Add files via upload

---
+++ word-list.js
@@ -0,0 +1,114 @@
+    const wordList = [
+    {
+        word: "kerajaan",
+        hint: "Kera apa yang paling besar"
+    },
+    {
+        word: "sate",
+        hint: "Kecil, kurus, lancip. Tapi banyak dagingnya."
+    },
+    {
+        word: "copet",
+        hint: "Dipanggil malah lari."
+    },
+    {
+        word: "ingus",
+        hint: "Turunnya pelan, naiknya cepat."
+    },
+    {
+        word: "muztahil",
+        hint: "Bahasa Arabnya Masjid Di Tengah Laut"
+    },
+    {
+        word: "koran",
+        hint: "Nenek-nenek jatuh di sungai, munculnya di mana?."
+    },
+    {
+        word: "jamsostek",
+        hint: "Jam yang sangat berharga bagi karyawan."
+    },
+    {
+        word: "unta",
+        hint: "Lahir di Arab, besar di Arab, tapi gak bisa Bahasa Arab."
+    },
+    {
+        word: "kandangnya",
+        hint: "Gajah besar apanya?."
+    },
+    {
+        word: "sendiri",
+        hint: "Saat naik pesawat kita dilarang membawa?"
+    },
+    {
+        word: "macan",
+        hint: "Seorang istri akan merasa terkejut bila di hari ulang tahun dibelikan?"
+    },
+    {
+        word: "matang",
+        hint: "Nasi yang enak buat sarapan, biasanya nasi?"
+    },
+    {
+        word: "lapar",
+        hint: "Binatang yang hinggap di makanan adalah binatang?"
+    },
+    {
+        word: "keramas",
+        hint: "Di dalam perpustakaan kita dilarang."
+    },
+    {
+        word: "lilin",
+        hint: "Benda apa yang dimakan tulangnya bukan dagingnya"
+    },
+    {
+        word: "bicycle",
+        hint: "A human-powered vehicle with two wheels."
+    },
+    {
+        word: "nasihat",
+        hint: "Nasi yang susah ditelan."
+    },
+    {
+        word: "umur",
+        hint: "Naiknya lambat dan tidak bisa turun."
+    },
+    {
+        word: "makan",
+        hint: "Kegiatan yang bisa menyelamatkan nyawa setiap hari"
+    },
+    {
+        word: "buku",
+        hint: "Punya halaman, tapi tidak punya rumah."
+    },
+    {
+        word: "piring",
+        hint: "Dibeli untuk makanan tetapi tidak pernah dimakan."
+    },
+    {
+        word: "payung",
+        hint: "Ditutup jadi tongkat, dibuka jadi tenda"
+    },
+    {
+        word: "konde",
+        hint: "Ditusuk bukan sate, dijaring bukan ikan"
+    },
+    {
+        word: "telur",
+        hint: "Dipecahkan terlebih dahulu, baru digunakan"
+    },
+    {
+        word: "rujak",
+        hint: "Jenis buah yang memiliki 10 rasa"
+    },
+    {
+        word: "satu",
+        hint: "Jika 1=5; 2=10; 3=20; 4=40; maka 5 = ?."
+    },
+    {
+        word: "rambut",
+        hint: " Masih kecil hitam, tetapi kalau sudah besar menjadi putih?."
+    },
+    {
+        word: "baju",
+        hint: "Punya lengan tapi tak punya jari, punya leher tapi tak punya kepala."
+    },
+];
